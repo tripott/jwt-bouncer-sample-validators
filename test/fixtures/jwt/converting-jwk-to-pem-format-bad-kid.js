8954341d451a0f18dfbc6c96d4605118d29790d0
tripott <tripott@gmail.com>
2019-03-14 15:40:13 -0400

    remove tenant

--- test/fixtures/jwt/converting-jwk-to-pem-format-bad-kid.js
+++ test/fixtures/jwt/converting-jwk-to-pem-format-bad-kid.js
@@ -1,8 +1,8 @@
-const params = { tenant: 'labs' }
+//const params = { tenant: 'labs' }
 // const params = { tenant: 'labs', id: 'view-medication-risk' }
 
 const req = {
-  params,
+  //params,
   whitelistValidatorResult: {
     ok: true,
     whiteListItem: {
